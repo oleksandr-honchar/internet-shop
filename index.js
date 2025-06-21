3cf3ed12ccaada07800065192a93d5e00f4df0e1
oleksandr-honchar <figules@gmail.com>
2025-06-21 07:48:39 +0000

    Deploying to gh-pages from @ oleksandr-honchar/internet-shop@57a2e48eb1b283ec2ed92601f1697fdbc45fb278 🚀

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-import"./assets/styles-CpGvQIKr.js";const d="https://dummyjson.com/products",f="/search?q=";document.querySelector(".categories");const r=document.querySelector(".products"),m=document.querySelector(".search-form__btn");let a=1;const n=12;async function i(t=1){try{const e=(t-1)*n;return await(await fetch(`${d}?limit=${n}&skip=${e}`)).json()}catch{throw new Error("Failed to load products")}}async function h(){try{const t=await i();if(!t.products.length){alert("No products found");return}r.insertAdjacentHTML("beforeend",c(t.products)),t.total>n&&y()}catch(t){alert(t.message)}}function y(){const t=document.createElement("button");t.textContent="Load more",t.classList.add("load-more-btn"),r.after(t),t.addEventListener("click",async()=>{a+=1;try{const e=await i(a);a*n>=e.total&&(t.style.display="none"),r.insertAdjacentHTML("beforeend",c(e.products))}catch(e){alert(e.message)}})}async function $(){const t=document.querySelector(".search-form__input").value.trim();if(!t)throw new Error("Please enter a search term");return await(await fetch(`${d}${f}${t}`)).json()}function P(){r.innerHTML=""}m.addEventListener("click",async t=>{t.preventDefault(),P();try{const e=await $();if(console.log(e),!e.products.length){alert("No products found for your search term");return}r.insertAdjacentHTML("beforeend",c(e.products))}catch(e){alert(e.message)}});function c(t){return t.map(({images:e,title:o,category:s,description:u,rating:l,price:p})=>`
-    <li class="product-card">
-      <img src="${e[0]}" alt="${o}" />
-      <div class="product-info">
-        <h3>${o}</h3>
-        <p>Category: ${s}</p>
-        <p>Description: ${u}</p>
-        <p>Rating: ${l}</p>
-        <p>Price: $${p}</p>
-        
-      </div>
-    </li>
-  `).join("")}h();
+import"./assets/styles-CpGvQIKr.js";import{a as r}from"./assets/vendor-BBQ7ZO7I.js";r.defaults.baseURL="https://dummyjson.com/";const o={categoryList:document.querySelector(".categories"),productsList:document.querySelector(".products"),searchForm:document.querySelector(".search-form"),searchInput:document.querySelector(".search-form__input"),searchBtn:document.querySelector(".search-form__btn")};o.categoryList.addEventListener("click",c);function c(e){if(e.target.nodeName!=="BUTTON")return;const t=e.target.textContent;console.log(`Selected category: ${t}`)}
 //# sourceMappingURL=index.js.map
